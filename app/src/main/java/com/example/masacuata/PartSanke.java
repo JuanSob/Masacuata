af7c1ea1aad4c1215afe050e0af2b6540726cf16
josefigueroa17 <josecarlosfigueroa919@gmail.com>
2021-11-24 22:43:25 -0600

    trabajando class PartSnake

--- app/src/main/java/com/example/masacuata/PartSanke.java
+++
@@ -1,4 +0,0 @@
-package com.example.masacuata;
-
-public class PartSanke {
-}
--- app/src/main/java/com/example/masacuata/Snake.java
+++ app/src/main/java/com/example/masacuata/Snake.java
@@ -1,4 +1,2 @@
-package com.example.masacuata;
-
-public class Snake {
+package com.example.masacuata;public class Snake {
 }
