e60ffcc90ee48c82a6d5290337ebdc2c553c337f
anme-22 <angeldavidmendez2012@gmail.com>
2021-11-26 11:42:35 -0600

    Compañeros añadí una documentacion

--- app/src/main/java/com/example/masacuata/MainActivity.java
+++ app/src/main/java/com/example/masacuata/MainActivity.java
@@ -7,7 +7,7 @@ import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-
+//Buenas tardes masturbanda
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
